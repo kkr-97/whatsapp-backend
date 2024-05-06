4b86d4cc4b046b3306d093e2544ab4560db71b3f
charan <charanpraveen9989@gmail.com>
2024-05-06 21:32:15 +0530

    ok'

--- api/main.py
+++ api/main.py
@@ -165,7 +165,7 @@ def send_whatsapp_messages():
         message = data['message']
     if 'details' in data:
         resultsOfMessages = []   
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver = webdriver.Chrome(service=Service('chrome.exe'))
         #intial connect to whatsapp
         driver.get('https://web.whatsapp.com')
         time.sleep(40)
