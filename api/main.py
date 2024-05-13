2f13f26374129ceaea129091736bb2e9a9dbc728
Katakam Charan Praveen Kumar <127504248+Idontnol@users.noreply.github.com>
2024-05-13 16:33:26 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -344,5 +344,5 @@ def sayHell():
 #         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8081)  # Set debug=False for production
+    app.run(debug=True)  # Set debug=False for production
     print("her poer 8081")
