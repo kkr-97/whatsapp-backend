415e445d2b817dc77b2214aef403ba4c5ad208b5
Katakam Charan Praveen Kumar <127504248+Idontnol@users.noreply.github.com>
2024-05-13 17:15:12 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -167,6 +167,7 @@ def send_whatsapp_messages():
         resultsOfMessages = []   
         chrome_driver_path="./chrome.exe"
         driver = webdriver.Chrome(service=ChromeDriverManager().install())
+        time.sleep(10)
         # driver=webdriver.Chrome(service=Service())
         #intial connect to whatsapp
         driver.get('https://web.whatsapp.com')
